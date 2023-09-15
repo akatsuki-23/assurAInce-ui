443ed433a98887f0840c26a749b46b0d821e279b
rithwikkeyvalue <rithwik@keyvalue.systems>
2023-09-15 17:26:53 +0530

    Integrate Welcome page

---
+++ src/containers/welcome/Welcome.jsx
@@ -0,0 +1,80 @@
+import { useNavigate } from 'react-router-dom';
+import Button from '../../components/button/Button';
+import { LogoIcon, PlusCircleIcon } from '../../components/icons';
+import { useRecoilState } from 'recoil';
+import { isExistingLoginAtom } from '../../store/atoms';
+
+const WelcomePage = () => {
+  const navigate = useNavigate();
+  const [, setIsExistingLogin] = useRecoilState(isExistingLoginAtom);
+
+  const onButtonClick = () => {
+    localStorage.setItem('isExistingLogin', 'true');
+
+    setIsExistingLogin(true);
+    // TODO:
+    // navigate('/employees') -> navigate accoridinly
+
+    navigate('/home');
+  }
+
+  return (
+    <div className="w-screen h-screen bg-[#F9FAFB] relative border-t-[6px] border-t-[#985EFF]">
+      {/* TODO: font for logo */}
+      <div className="flex flex-row items-center gap-[6px] py-2 absolute top-[68px] left-[82px]">
+        <LogoIcon className="flex items-center" />
+        <div className="text-[37px] font-medium items-center leading-[45px] text-[#1B1818]">
+          AssurAInce
+        </div>
+      </div>
+      <div className="flex flex-col w-full h-full justify-center items-center">
+        <img src="images/welcome-img.png" alt="" className="mb-[70px]" />
+        <div className="font-semibold text-[24px] leading-[28px] mb-[4px]">
+          Welcome Jestin
+        </div>
+        <div className="text-[16px] text-[#475367] mb-[45px]">
+          AI-powered dashboard for streamlined insights and Know Employee
+          Performance
+        </div>
+        <div className="flex gap-[14px] items-center justify-center w-full">
+          <Button
+            onClick={onButtonClick}
+            hoverBgColor="#985EFF"
+            style={{
+              borderRadius: '6px',
+              height: '36px',
+              backgroundColor: '#985EFF',
+              padding: '8px 12px',
+            }}
+          >
+            <div className="flex gap-[8px] items-center">
+              <PlusCircleIcon fill="#FFFFFF" />
+              <div className="text-white text-[14px] font-semibold">
+                Add Employee
+              </div>
+            </div>
+          </Button>
+          <Button
+            onClick={onButtonClick}
+            hoverBgColor="#0B111C"
+            style={{
+              borderRadius: '6px',
+              height: '36px',
+              backgroundColor: 'black',
+              padding: '8px 12px',
+            }}
+          >
+            <div className="flex gap-[8px] items-center">
+              <PlusCircleIcon fill="#FFFFFF" />
+              <div className="text-white text-[14px] font-semibold">
+                Onboard AI
+              </div>
+            </div>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WelcomePage;
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,31 +1,49 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from 'react-router-dom';
 
-import RoutesPath from "./RoutesPath";
-import HomePage from "../containers/home/Home";
-import NavigationBar from "../components/nav-bar/NavigationBar";
-import Notification from "../features/notification/Notification";
-import Topbar from "../components/top-bar/Topbar";
+import RoutesPath from './RoutesPath';
+import HomePage from '../containers/home/Home';
+import NavigationBar from '../components/nav-bar/NavigationBar';
+import Notification from '../features/notification/Notification';
+import Topbar from '../components/top-bar/Topbar';
+import WelcomePage from '../containers/welcome/Welcome';
+import { useEffect } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { isExistingLoginAtom } from '../store/atoms';
 
 const PrivateLayout = () => {
-  return (
+  const [, setIsExistingLogin] = useRecoilState(isExistingLoginAtom);
+  const isExistingLogin = useRecoilValue(isExistingLoginAtom);
+
+  useEffect(() => {
+    const isExisting = localStorage.getItem('isExistingLogin') === 'true';
+
+    setIsExistingLogin(isExisting);
+  }, []);
+
+  return isExistingLogin ? (
     <div className="flex h-screen">
       <NavigationBar />
       <div className="flex w-screen flex-col">
-      <Topbar/>
-      <div className="flex h-screen flex-row overflow-y-auto bg-[#F9FAFB] justify-center">
-        <div className="w-[calc(100vw-272px)]">
-          <Routes>
-            <Route path={RoutesPath.HOME} element={<HomePage />} />
-            <Route
-              path={RoutesPath.ALL}
-              element={<Navigate to={RoutesPath.HOME} replace />}
-            />
-          </Routes>
+        <Topbar />
+        <div className="flex h-screen flex-row overflow-y-auto bg-[#F9FAFB] justify-center">
+          <div className="w-[calc(100vw-272px)]">
+            <Routes>
+              <Route path={RoutesPath.HOME} element={<HomePage />} />
+              <Route
+                path={RoutesPath.ALL}
+                element={<Navigate to={RoutesPath.HOME} replace />}
+              />
+            </Routes>
+          </div>
+          <Notification />
         </div>
-      </div>
       </div>
       <Notification />
     </div>
+  ) : (
+    <Routes>
+      <Route path={RoutesPath.ALL} element={<WelcomePage />} />
+    </Routes>
   );
 };
 
