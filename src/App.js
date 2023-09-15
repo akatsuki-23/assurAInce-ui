214b84a76ff7cf2803955ab7cfe7912648d72d08
cizil-keyvalue <109058674+cizil-keyvalue@users.noreply.github.com>
2023-09-15 10:16:44 +0530

    feat(navigation): navigation bar initial change (#1)

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import { RecoilRoot } from "recoil";
 import "./styles/global.css";
 import Layout from "./routes";
 
-import './styles/global.css';
-import Notification from './features/notification/Notification';
+import "./styles/global.css";
+import Notification from "./features/notification/Notification";
+import NavigationBar from "./components/nav-bar/NavigationBar";
 
 function App() {
   return (
-      <RecoilRoot>
-        <Layout />
-        <Notification />
-      </RecoilRoot>
+    <RecoilRoot>
+      <div className="flex h-screen w-screen flex-row overflow-y-auto">
+      <NavigationBar />
+      <Layout />
+      </div>
+      <Notification />
+    </RecoilRoot>
   );
 }
 
--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,4 +1,6 @@
 export { ReactComponent as SampleIcon } from "../../assets/icons/sample.svg";
 export { ReactComponent as SearchIcon } from "../../assets/icons/searchIcon.svg";
 export { ReactComponent as RoundedTickIcon } from "../../assets/icons/roundedTick.svg";
-export { ReactComponent as RoundedErrorIcon} from "../../assets/icons/roundedError.svg";
+export { ReactComponent as RoundedErrorIcon } from "../../assets/icons/roundedError.svg";
+export { ReactComponent as LogoIcon } from "../../assets/icons/logo.svg";
+export { ReactComponent as PlusIcon } from "../../assets/icons/plusIcon.svg";
