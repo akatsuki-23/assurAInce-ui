133780602d3c04fc2a3324d9ec4ecc678681639e
JESTIN ROY <roy_jestin@yahoo.com>
2023-09-16 06:47:56 +0530

    fix: Fixed the bug in the Employee

--- src/components/carousel/EmployeeCarousel.jsx
+++ src/components/carousel/EmployeeCarousel.jsx
@@ -1,42 +1,45 @@
-import Slider from 'react-slick';
+import Slider from "react-slick";
 
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const Card = ({data}) => {
+const Card = ({ data }) => {
   return (
-    <div className='mr-[23px]'>
-      <div className='items-center w-full h-[72px]  border border-[#E4E7EC] rounded-[12px] p-[16px] flex gap-[12px] bg-white pr-8'>
-        <img src={data?.profileUrl ?? 'https://picsum.photos/200'} alt="" className="w-[40px] h-[40px] rounded-full" />
-        <div className='flex flex-col'>
-          <div className='font-medium text-[14px] text-[#101928]'>
+    <div className="mr-[23px]">
+      <div className="items-center w-full h-[72px]  border border-[#E4E7EC] rounded-[12px] p-[16px] flex gap-[12px] bg-white pr-8">
+        <img
+          src={data?.profileUrl ?? "https://picsum.photos/200"}
+          alt=""
+          className="w-[40px] h-[40px] rounded-full"
+        />
+        <div className="flex flex-col">
+          <div className="font-medium text-[14px] text-[#101928]">
             {data?.firstName} {data?.lastName}
           </div>
-          <div className='font-normal text-[14px] text-[#475367]'>
-           {data?.careerDetails ?? 'Graphic Designer'}
+          <div className="font-normal text-[14px] text-[#475367]">
+            {data?.careerDetails ?? "Graphic Designer"}
           </div>
         </div>
       </div>
     </div>
   );
-}; 
+};
 
-const EmployeeCarousel = ({data}) => {
-
-  const slides = data?.length > 1 ? 4 : 1;
+const EmployeeCarousel = ({ data }) => {
+  const slides = data?.length > 3 ? 4 : data?.length;
   const settings = {
-    slidesToShow: slides ,
+    slidesToShow: slides,
     slidesToScroll: 1,
+    isFinite: data?.length > 3 ? true : false,
+    className: data?.length > 3 ? "" : "max-w-[231px]",
   };
 
-
   return (
-      <Slider {...settings}>
-        {data?.map((item) => (
-          <Card key={item.id} data={item}/>
-        ))}
-      </Slider>
-
+    <Slider {...settings}>
+      {data?.map((item) => (
+        <Card key={item.id} data={item} />
+      ))}
+    </Slider>
   );
 };
 
